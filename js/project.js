1c67e028f6c617bac6ef78d4c820e2798c336370
Maikel Koopman <maikel.koopman@goalgorilla.com>
2016-02-03 14:00:09 +0100

    Update 2016-02-03T13:00:07.042Z

--- js/project.js
+++ js/project.js
@@ -2,20 +2,11 @@ jQuery(document).ready(function($){
 
   'use strict';
 
-  $('.menu-open').on('click', function(e){
-    $('body').toggleClass('menu-panel-isOpen');
-    e.preventDefault();
-  });
-
-  $('.b-menu').on('click', function(e) {
-    $('.b-container').toggleClass('open');
-    e.preventDefault();
-  });
-
-
   $('.site-logo').click(function(event){
     e.preventDefault();
     $("html, body").animate({ scrollTop: 0 });
   });
+
+  new UISearch( document.getElementById( 'search-wrapper' ) );
 
 });
