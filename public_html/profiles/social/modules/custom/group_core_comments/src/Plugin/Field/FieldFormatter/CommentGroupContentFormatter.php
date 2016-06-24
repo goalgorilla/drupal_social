6b636d903017aced035f371e9f33e79664ea81bc
Taras Kruts <taras.kruts@lemberg.co.uk>
2016-06-24 14:47:59 +0200

    Issue #2747585 by ribel: Add Comment Group Content field formatter

---
+++ public_html/profiles/social/modules/custom/group_core_comments/src/Plugin/Field/FieldFormatter/CommentGroupContentFormatter.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Drupal\group_core_comments\Plugin\Field\FieldFormatter;
+
+use Drupal\comment\Plugin\Field\FieldFormatter\CommentDefaultFormatter;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\group\Entity\GroupContent;
+
+
+/**
+ * Plugin implementation of the 'comment_group_content' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "comment_group_content",
+ *   label = @Translation("Comment on group content"),
+ *   field_types = {
+ *     "comment"
+ *   }
+ * )
+ */
+class CommentGroupContentFormatter extends CommentDefaultFormatter {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $parent = parent::viewElements($items, $langcode);
+    $entity = $items->getEntity();
+    $group_contents = GroupContent::loadByEntity($entity);
+
+    if (!empty($group_contents)) {
+      // Add cache contexts.
+      $parent['#cache']['contexts'][] = 'group.type';
+      $parent['#cache']['contexts'][] = 'group_membership';
+
+      $account = \Drupal::currentUser();
+      $renderer = \Drupal::service('renderer');
+
+      foreach ($group_contents as $group_content) {
+        $group = $group_content->getGroup();
+        // Add cacheable dependency.
+        $membership = $group->getMember($account);
+        $renderer->addCacheableDependency($parent, $membership);
+        // Remove comments from output if user don't have access.
+        if (!$group->hasPermission('access comments', $account)) {
+          unset($parent[0]);
+        }
+      }
+    }
+    return $parent;
+  }
+
+}
