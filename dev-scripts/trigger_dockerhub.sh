0cbb8e836d2ff37c30057c58173f9146a2ebb501
Niels van der Molen <nielsvandermolen@goalgorilla.com>
2016-08-23 11:29:14 +0200

    DS-1847 by nielsvandermolen: Update DockerHub script so it will only trigger on the 8.x-1.x branch

--- dev-scripts/trigger_dockerhub.sh
+++ dev-scripts/trigger_dockerhub.sh
@@ -3,5 +3,7 @@
 # This script is used in Travis in order to trigger an automated build in the DockerHub.
 # Key is secure so it can not be used in forks. Currently we only trigger on the 8.x-1.x branch.
 
-echo $TRAVIS_BRANCH;
-curl -H "Content-Type: application/json" --data '{"build": true}' -X POST https://registry.hub.docker.com/u/goalgorilla/drupal_social/trigger/$DOCKERHUB_TOKEN/
+if [[ $TRAVIS_BRANCH == "8.x-1.x" ]]
+then
+  curl -H "Content-Type: application/json" --data '{"build": true}' -X POST https://registry.hub.docker.com/u/goalgorilla/drupal_social/trigger/$DOCKERHUB_TOKEN/
+fi
