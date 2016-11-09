f99be546bb9feeb5686959d08a57d36bd85263f2
jaapjan <mail@jaapjan.nl>
2016-11-09 10:09:54 +0100

    Only trigger the travis build on the 8.x-1.x branch.

--- dev-scripts/trigger_travis.sh
+++ dev-scripts/trigger_travis.sh
@@ -1,17 +1,24 @@
 #!/usr/bin/env bash
 
-ACCESS_TOKEN=$TRAVIS_ACCESS_TOKEN
-REPO='open_social_testing';
+# This script is used in Travis in order to trigger an automated build in the Travis of the open_social_testing repo.
+# Key is secure so it can not be used in forks. Currently we only trigger on the 8.x-1.x branch.
+if [[ $TRAVIS_BRANCH == "8.x-1.x" ]] && [[ $TRAVIS_PULL_REQUEST == "false" ]]
+then
 
-body='{
-"request": {
-  "branch":"master"
-}}'
+    ACCESS_TOKEN=$TRAVIS_ACCESS_TOKEN
+    REPO='open_social_testing';
 
-curl -s -X POST \
-  -H "Content-Type: application/json" \
-  -H "Accept: application/json" \
-  -H "Travis-API-Version: 3" \
-  -H "Authorization: token $ACCESS_TOKEN" \
-  -d "$body" \
-  https://api.travis-ci.org/repo/goalgorilla%2F$REPO/requests
+    body='{
+    "request": {
+      "branch":"master"
+    }}'
+
+    curl -s -X POST \
+      -H "Content-Type: application/json" \
+      -H "Accept: application/json" \
+      -H "Travis-API-Version: 3" \
+      -H "Authorization: token $ACCESS_TOKEN" \
+      -d "$body" \
+      https://api.travis-ci.org/repo/goalgorilla%2F$REPO/requests
+
+fi
