61136baa89c420f5703c864c383d5a1f48a46361
Niels van der Molen <nielsvandermolen@goalgorilla.com>
2016-11-30 16:43:53 +0100

    Trigger the new Dockerhub repo

--- dev-scripts/trigger_dockerhub.sh
+++ dev-scripts/trigger_dockerhub.sh
@@ -4,5 +4,5 @@
 # Key is secure so it can not be used in forks. Currently we only trigger on the 8.x-1.x branch.
 if [[ $TRAVIS_BRANCH == "8.x-1.x" ]] && [[ $TRAVIS_PULL_REQUEST == "false" ]]
 then
-  curl -H "Content-Type: application/json" --data '{"build": true}' -X POST https://registry.hub.docker.com/u/goalgorilla/drupal_social/trigger/$DOCKERHUB_TOKEN/
+  curl -H "Content-Type: application/json" --data '{"build": true}' -X POST https://registry.hub.docker.com/u/goalgorilla/open_social_docker/trigger/$DOCKERHUB_TOKEN/
 fi
